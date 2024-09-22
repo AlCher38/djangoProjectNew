80e2db23cf900a112a47f2220c2cb28330f95d62
Alex <al.cher2023@icloud.com>
2024-09-22 11:21:41 +0800

    New_Project

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
-
 from catalog.models import Category, Product
 
 
-# admin.site.register(Student)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "category")
+    list_filter = ("price", "name", "category")
+    search_fields = ("name", "description")
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
-    list_filter = ("name",)
-    search_fields = ("name",)
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name",)
--- catalog/models.py
+++ catalog/models.py
@@ -1,32 +1,53 @@
+from decimal import Decimal
 from django.db import models
 
 
-# NULLABLE = {'blank': True, 'null': True}
+BLANK_NULL_TRUE = {"blank": True, "null": True}
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    """
+    A model for storing product categories.
+    """
 
-    class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
-
-
-class Product(models.Model):
-    Category,
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to="images/")
-    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.FloatField()
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    name = models.CharField(max_length=50, verbose_name="Наименование категории")
+    description = models.TextField(verbose_name="Описание категории")
 
     def __str__(self):
         return self.name
 
     class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
         ordering = ["name"]
-        verbose_name = "Продукт"
-        verbose_name_plural = "Продукты"
+
+
+class Product(models.Model):
+    """
+    A model for storing information about products.
+    """
+
+    name = models.CharField(max_length=50, verbose_name="Наименование товара")
+    description = models.TextField(**BLANK_NULL_TRUE, verbose_name="Описание товара")
+    preview = models.ImageField(
+        **BLANK_NULL_TRUE, verbose_name="Изображение товара"
+    )
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=Decimal("0.00"),
+        verbose_name="Цена за покупку",
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["name", "price", "created_at", "updated_at"]
